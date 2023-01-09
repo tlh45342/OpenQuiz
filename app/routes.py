43b370b84c14e9148df0a576141d1ccc6c9bb7b4
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2023-01-09 11:44:05 -0500

    Create routes.py

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from flask import render_template, request, flash, session, url_for, redirect
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from app import app
+
+
+@app.route('/', methods=['GET'])
+def root():
+  if current_user.is_authenticated:
+     return render_template("home.html")
+  return render_template("login.html")
+
+
+@app.route("/protected",)
+@login_required
+def protected():
+    return render_template("protected.html")
+
+
+@app.errorhandler(404)
+def invalid_route(e):
+  return render_template("404.html")
